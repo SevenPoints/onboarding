25b90df8c8bbd2bf1fa4eec58fccdd60dc3fb418
Prelang Builder <builder@prelang.com>
2016-03-31 00:59:51 +0000

    Generated model Company with migration

---
+++ spec/factories/companies.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+  factory :company do
+    name "MyString"
+    street_1 "MyString"
+    street_2 "MyString"
+    city "MyString"
+    state "MyString"
+    zip_code "MyString"
+    country "MyString"
+  end
+end
