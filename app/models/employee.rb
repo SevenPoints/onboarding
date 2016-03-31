9e175646bc5e576a0d0be3c6d4f5a8adb91b13d9
Prelang Builder <builder@prelang.com>
2016-03-31 00:59:57 +0000

    Defined validation for attribute 'mobile' on model 'Employee'

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,4 +2,5 @@ class Employee < ActiveRecord::Base
   belongs_to :company
   validates_formatting_of :email, using: :email
   validates_formatting_of :work_phone, using: :us_phone
+  validates_formatting_of :mobile, using: :us_phone
 end
